f26f294424a9a11720e90c205640db97b50e86d0
Satia7364 <71262127+Satia7364@users.noreply.github.com>
2023-11-24 10:08:29 +0800

    day-2
    程式

--- homework-2.py
+++ homework-2.py
@@ -1,1 +1,13 @@
-print("Hello World")
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age= age
+
+    def __str__(self):
+        return f"Name: {self.name}\nAge = {self.age}"
+    
+p1 = Person("John", 36)
+print(p1)
+
+p2 = Person("Sue", 22)
+print(p2.__dict__)
